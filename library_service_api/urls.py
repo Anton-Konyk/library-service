d23e5cd3627fb2a5d870dae020dbbcd91a0d4116
Anton Konyk <antonkonyk.com>
2024-09-26 14:59:21 +0200

    change path for books.urls

--- library_service_api/urls.py
+++ library_service_api/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/book/", include("books.urls"), name="book"),
+    path("api/v1/book-service/", include("books.urls"), name="book"),
 ]
