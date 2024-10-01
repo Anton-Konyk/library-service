1d8e22f59576f047cec306bec824fa5351e09135
Anton Konyk <antonkonyk.com>
2024-10-01 20:17:42 +0200

    create BorrowingCreateSerializer

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -20,3 +20,17 @@ class BorrowingListSerializer(serializers.ModelSerializer):
             "book",
             "user",
         )
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user",
+        )
