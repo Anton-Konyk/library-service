9bd3c4a08e79b49560915ecbb4246d574ade5107
Anton Konyk <antonkonyk.com>
2024-10-02 20:29:48 +0200

    create endpoint path for BorrowingReturnView

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from borrowings.views import BorrowingViewSet
+from borrowings.views import BorrowingViewSet, BorrowingReturnView
 
 router = routers.DefaultRouter()
 router.register("borrowings", BorrowingViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("borrowings/<int:id>/return/", BorrowingReturnView.as_view(), name="return"),
 ]
 
 app_name = "borrowings"
