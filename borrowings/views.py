67fad19ee7e180a229bdbeaa105ee4842525b1b5
Anton Konyk <antonkonyk.com>
2024-10-02 17:59:08 +0200

    create get_queryset with conditions for action list and non staff users

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
@@ -12,6 +13,13 @@ class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingListSerializer
     permission_classes = (IsAuthenticated,)
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.action == "list" and not self.request.user.is_staff:
+            queryset = self.queryset.filter(user=self.request.user)
+            return queryset
+
+        return queryset
 
     def get_serializer_class(self):
         if self.action == "list" or self.action == "retrieve":
