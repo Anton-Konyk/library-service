c26ddf9cee76edb8867e2de2056a9f4047772016
Anton Konyk <antonkonyk.com>
2024-10-03 20:48:54 +0200

    create TelegramHelper

---
+++ helpers/telegram_helper.py
@@ -0,0 +1,35 @@
+import os
+
+import requests
+
+from dotenv import load_dotenv
+
+
+class TelegramHelper:
+    """
+    Class for sending messages via Telegram API.
+    """
+
+    load_dotenv()
+
+    def __init__(self):
+        self.token = os.getenv("TELEGRAM_BOT_TOKEN")
+        self.chat_id = os.getenv("TELEGRAM_CHAT_ID")
+        self.api_url = f"https://api.telegram.org/bot{self.token}/sendMessage"
+
+    def send_message(self, message):
+        payload = {"chat_id": self.chat_id, "text": message}
+        print(f"Chat: {self.chat_id}")
+        try:
+            response = requests.post(self.api_url, data=payload)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.Timeout:
+            print("Error: timeout request")
+            return None
+        except requests.exceptions.HTTPError as http_err:
+            print(f"HTTP error occurred: {http_err}")
+            return None
+        except requests.exceptions.RequestException as e:
+            print(f"Error sending message to Telegram: {e}")
+            return None
