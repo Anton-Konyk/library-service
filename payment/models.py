ba55d63ee487a1ffe6274987c4397336bf17b33d
Anton Konyk <antonkonyk.com>
2024-10-07 20:57:39 +0200

    create payment model

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+from borrowings.models import Borrowing
+
+
+STATUS_CHOICES = (
+    ("G", "PENDING"),
+    ("D", "PAID"),
+)
+
+TYPE_CHOICES = (
+    ("P", "PAYMENT"),
+    ("F", "FINE"),
+)
+
+
+class Payment(models.Model):
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default="G")
+    type = models.CharField(max_length=1, choices=TYPE_CHOICES, default="P")
+    borrowing = models.ForeignKey(
+        Borrowing, on_delete=models.CASCADE, related_name="payments"
+    )
+    session_url = models.URLField()
+    session_id = models.CharField(max_length=120)
+    money = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.borrowing.id} {self.status} {self.type} {self.money}"
+
+    class Meta:
+        verbose_name_plural = "payments"
+        ordering = [
+            "money",
+        ]
